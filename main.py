c70cb507b312acc3c6bb7e1fd22e2b95414cded1
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2024-09-19 06:03:06 -0300

    chore: initi projetc

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/home/tiago/.pyenv/shims/python
+
+def distribuir_corrida(D_total, V_inicial, C, razao):
+    distancias = []
+    velocidades = []
+    
+    # Calcular a soma total da progressão geométrica para normalizar as distâncias
+    soma_geometrica = sum(razao ** (C - i) for i in range(1, C + 1))
+    
+    # Para cada ciclo, calcular a distância e a velocidade
+    for n in range(1, C + 1):
+        # Distância proporcional
+        distancia = D_total * (razao ** (C - n)) / soma_geometrica
+        distancias.append(distancia)
+        
+        # Velocidade proporcional (aumenta a cada ciclo)
+        velocidade = V_inicial / (razao ** (n - 1))
+        velocidades.append(velocidade)
+    
+    return distancias, velocidades
+
+
+
+def exec(D_total, V_inicial, C, razao):
+    distancias, velocidades = distribuir_corrida(D_total, V_inicial, C, razao)
+
+# Exibindo os resultados
+    etapa=0
+    for i, (dist, vel) in enumerate(zip(distancias, velocidades), start=1):
+        etapa+=dist
+        print(f"Ciclo {i}: Distância = {dist:.2f} m, Velocidade = {(vel*1000)/60:.6f} s/m - etapa {etapa}")
+
+
+
+def main():
+    print('configuere sua corrida')
+
+    distanciaTotalInformada = input('Distância total ')
+    distanciaTotal = int(distanciaTotalInformada)
+
+    quantidadeCiclos = int(input('Informe a quantidade de ciclos'))
+
+    V_inicial = 360 / 1000  # Tempo por metro no primeiro ciclo (300s/1000m)
+    razao = 1.04  # Razão da progressão geométrica para aumentar a velocidade
+    exec(distanciaTotal, V_inicial, quantidadeCiclos, razao)
+
+if __name__ == "__main__":
+    main()
